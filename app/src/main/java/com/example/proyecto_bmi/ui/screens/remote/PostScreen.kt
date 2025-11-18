b04c27a05a9af573b695aaa6769042ae8814b209
Jeff <jeffersonrojasmeza15@hotmail.com>
2025-11-18 10:40:17 -0300

    Fixed PostScreen.kt

--- app/src/main/java/com/example/proyecto_bmi/ui/screens/remote/PostScreen.kt
+++ app/src/main/java/com/example/proyecto_bmi/ui/screens/remote/PostScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
